90951084949e39349290e0c87bdce1b0339f72a7
mraw <marwintos@gmail.com>
2017-04-22 01:03:12 +0200

    somaproc

--- resources/views/master1.blade.php
+++ resources/views/master1.blade.php
@@ -62,7 +62,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 			<div class="w3_agileits_banner_main_grid">
 				<div class="w3_agile_logo">
 				<br><br><br><br>
-					
+					<h1><a href="index"><span>S</span>OMAPROC<i><hr  style="color: #00b300";>Sté des Marchés de Production</i></a></h1>
 				</div>
 				<div class="agile_social_icons_banner">
 					<ul class="agileits_social_list">
