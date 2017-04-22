814a0deb042511e542267590d7810d9e3773659a
mraw <marwintos@gmail.com>
2017-04-22 16:23:53 +0200

    somaproc

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -90,8 +90,46 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 			</div>
 			
 		
-		<!-- welcome -->
 
+
+<br><br><br>
+			<div class="thim-click-to-bottom">
+				<a href="#welcome_bottom" class="scroll">
+					<i class="fa  fa-chevron-down"></i>
+				</a>
+			</div>
+		</div>
+	</div>
+<!-- banner -->
+
+<!-- banner -->
+	<div class="banner">
+
+		<div class="container">
+			<div class="w3_agileits_banner_main_grid">
+			<div class="w3_banner_info">
+			<br><br>
+				<div class="w3_banner_info_grid">
+					<h3 style="font-size: 250%;color: #fff;" align="center">Société des Marchés de production<br> du centre</h3>
+				    <h3 class="test" align="center">Votre meilleur choix</h3>
+				   
+					<br><br><br>
+					<ul>
+						<li><a href="contact" class="w3l_contact">Contactez nous</a></li>
+						<li><a href="#" class="w3ls_more" data-toggle="modal" data-target="#myModal">Lire la suite</a></li>
+					</ul>
+				</div>
+			</div>
+
+	<!-- welcome -->
+	<div class="welcome">
+		<div class="container">
+			<h3 class="agileits_w3layouts_head"  style="font-size: 250%;color: #fff;">Bienvenue à <span>Somaproc</span> Produits</h3>
+			<div class="w3_agile_image">
+				<img src="images/1.png" alt=" " class="img-responsive" />
+			</div>
+			
+		</div>
 		<div class="mis-stage w3_agileits_welcome_grids"> 
 			<!-- The element to select and apply miSlider to - the class is optional -->
 			<ol class="mis-slider">
@@ -134,25 +172,17 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 				</li>
 			</ol>
 		</div>
-	
+	</div>
 <!-- //welcome -->
-
-			
-				<div class="w3_banner_info_grid">
-					<h3 style="font-size: 250%;color: #fff;" align="center">Société des Marchés de production<br> du centre</h3>
-				    <h3 class="test" align="center">Votre meilleur choix</h3>
-				   
-					<ul>
-						<li><a href="contact" class="w3l_contact">Contactez nous</a></li>
-						<li><a href="#" class="w3ls_more" data-toggle="modal" data-target="#myModal">Lire la suite</a></li>
-					</ul>
-				</div>
+<br><br><br>
+			<div class="thim-click-to-bottom">
+				<a href="#welcome_bottom" class="scroll">
+					<i class="fa  fa-chevron-down"></i>
+				</a>
 			</div>
-			</div>
-			
+		</div>
+	</div>
 <!-- banner -->
-
-
 
 <!-- bootstrap-pop-up -->
 	<div class="modal video-modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModal">
