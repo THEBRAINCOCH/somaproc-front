5abbb80fa3e36b9deff3f2664504f72438c5a452
mraw <marwintos@gmail.com>
2017-04-22 15:26:03 +0200

    somaproc

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -1,6 +1,126 @@
 @extends('master')
 
 @section('content')
+<!-- banner-bottom -->
+	<div class="banner-bottom">
+		<div class="col-md-4 agileits_banner_bottom_left">
+			<div class="agileinfo_banner_bottom_pos">
+				<div class="w3_agileits_banner_bottom_pos_grid">
+					<div class="col-xs-4 wthree_banner_bottom_grid_left">
+						<div class="agile_banner_bottom_grid_left_grid hvr-radial-out">
+							<i class="fa fa-pagelines" aria-hidden="true"></i>
+						</div>
+					</div>
+					<div class="col-xs-8 wthree_banner_bottom_grid_right">	
+						<h4>Free Consultation</h4>
+						<p>Morbi viverra lacus commodo felis semper, eu iaculis lectus feugiat.</p>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4 agileits_banner_bottom_left1">
+			<div class="agileinfo_banner_bottom_pos">
+				<div class="w3_agileits_banner_bottom_pos_grid">
+					<div class="col-xs-4 wthree_banner_bottom_grid_left">
+						<div class="agile_banner_bottom_grid_left_grid hvr-radial-out">
+							<i class="fa fa-certificate" aria-hidden="true"></i>
+						</div>
+					</div>
+					<div class="col-xs-8 wthree_banner_bottom_grid_right">	
+						<h4>Certified Products</h4>
+						<p>Morbi viverra lacus commodo felis semper, eu iaculis lectus feugiat.</p>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4 agileits_banner_bottom_left2">
+			<div class="agileinfo_banner_bottom_pos">
+				<div class="w3_agileits_banner_bottom_pos_grid">
+					<div class="col-xs-4 wthree_banner_bottom_grid_left">
+						<div class="agile_banner_bottom_grid_left_grid hvr-radial-out">
+							<i class="fa fa-yelp" aria-hidden="true"></i>
+						</div>
+					</div>
+					<div class="col-xs-8 wthree_banner_bottom_grid_right">	
+						<h4>Free Helpline</h4>
+						<p>Morbi viverra lacus commodo felis semper, eu iaculis lectus feugiat.</p>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+		</div>
+		<div class="clearfix"> </div>
+	</div>
+<!-- //banner-bottom -->
+<!-- news -->
+	<div class="welcome">
+		<div class="container">
+			<h3 class="agileits_w3layouts_head">Latest <span>News</span> from plantation</h3>
+			<div class="w3_agile_image">
+				<img src="images/1.png" alt=" " class="img-responsive">
+			</div>
+			<p class="agile_para">Morbi viverra lacus commodo felis semper, eu iaculis lectus nulla at sapien blandit sollicitudin.</p>
+			<div class="w3ls_news_grids">
+				<div class="col-md-4 w3ls_news_grid">
+					<div class="w3layouts_news_grid">
+						<img src="images/3.jpg" alt=" " class="img-responsive" />
+						<div class="w3layouts_news_grid_pos">
+							<div class="wthree_text"><h3>plantation</h3></div>
+						</div>
+					</div>
+					<div class="agileits_w3layouts_news_grid">
+						<ul>
+							<li><i class="fa fa-calendar" aria-hidden="true"></i>25 March 2017</li>
+							<li><i class="fa fa-user" aria-hidden="true"></i><a href="#">Admin</a></li>
+						</ul>
+						<h4><a href="#" data-toggle="modal" data-target="#myModal">Design & Planting</a></h4>
+						<p>Vivamus lacinia odio ut euismod eleifend. Cum sociis natoque penatibus et 
+							magnis dis parturient montes.</p>
+					</div>
+				</div>
+				<div class="col-md-4 w3ls_news_grid">
+					<div class="w3layouts_news_grid">
+						<img src="images/6.jpg" alt=" " class="img-responsive" />
+						<div class="w3layouts_news_grid_pos">
+							<div class="wthree_text"><h3>plantation</h3></div>
+						</div>
+					</div>
+					<div class="agileits_w3layouts_news_grid">
+						<ul>
+							<li><i class="fa fa-calendar" aria-hidden="true"></i>28 March 2017</li>
+							<li><i class="fa fa-user" aria-hidden="true"></i><a href="#">Admin</a></li>
+						</ul>
+						<h4><a href="#" data-toggle="modal" data-target="#myModal">Quality & Reliability</a></h4>
+						<p>Vivamus lacinia odio ut euismod eleifend. Cum sociis natoque penatibus et 
+							magnis dis parturient montes.</p>
+					</div>
+				</div>
+				<div class="col-md-4 w3ls_news_grid">
+					<div class="w3layouts_news_grid">
+						<img src="images/5.jpg" alt=" " class="img-responsive" />
+						<div class="w3layouts_news_grid_pos">
+							<div class="wthree_text"><h3>plantation</h3></div>
+						</div>
+					</div>
+					<div class="agileits_w3layouts_news_grid">
+						<ul>
+							<li><i class="fa fa-calendar" aria-hidden="true"></i>30 March 2017</li>
+							<li><i class="fa fa-user" aria-hidden="true"></i><a href="#">Admin</a></li>
+						</ul>
+						<h4><a href="#" data-toggle="modal" data-target="#myModal">Satisfied Customers</a></h4>
+						<p>Vivamus lacinia odio ut euismod eleifend. Cum sociis natoque penatibus et 
+							magnis dis parturient montes.</p>
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+<!-- //news -->
+
+
 <!-- welcome-bottom -->
 	<div id="welcome_bottom" class="welcome-bottom">
 		<div class="col-md-6 wthree_welcome_bottom_left">
@@ -72,125 +192,6 @@
 		<div class="clearfix"> </div>
 	</div>
 <!-- //welcome-bottom -->
-<!-- news -->
-	<div class="welcome">
-		<div class="container">
-			<h3 class="agileits_w3layouts_head">Latest <span>News</span> from plantation</h3>
-			<div class="w3_agile_image">
-				<img src="images/1.png" alt=" " class="img-responsive">
-			</div>
-			<p class="agile_para">Morbi viverra lacus commodo felis semper, eu iaculis lectus nulla at sapien blandit sollicitudin.</p>
-			<div class="w3ls_news_grids">
-				<div class="col-md-4 w3ls_news_grid">
-					<div class="w3layouts_news_grid">
-						<img src="images/3.jpg" alt=" " class="img-responsive" />
-						<div class="w3layouts_news_grid_pos">
-							<div class="wthree_text"><h3>plantation</h3></div>
-						</div>
-					</div>
-					<div class="agileits_w3layouts_news_grid">
-						<ul>
-							<li><i class="fa fa-calendar" aria-hidden="true"></i>25 March 2017</li>
-							<li><i class="fa fa-user" aria-hidden="true"></i><a href="#">Admin</a></li>
-						</ul>
-						<h4><a href="#" data-toggle="modal" data-target="#myModal">Design & Planting</a></h4>
-						<p>Vivamus lacinia odio ut euismod eleifend. Cum sociis natoque penatibus et 
-							magnis dis parturient montes.</p>
-					</div>
-				</div>
-				<div class="col-md-4 w3ls_news_grid">
-					<div class="w3layouts_news_grid">
-						<img src="images/6.jpg" alt=" " class="img-responsive" />
-						<div class="w3layouts_news_grid_pos">
-							<div class="wthree_text"><h3>plantation</h3></div>
-						</div>
-					</div>
-					<div class="agileits_w3layouts_news_grid">
-						<ul>
-							<li><i class="fa fa-calendar" aria-hidden="true"></i>28 March 2017</li>
-							<li><i class="fa fa-user" aria-hidden="true"></i><a href="#">Admin</a></li>
-						</ul>
-						<h4><a href="#" data-toggle="modal" data-target="#myModal">Quality & Reliability</a></h4>
-						<p>Vivamus lacinia odio ut euismod eleifend. Cum sociis natoque penatibus et 
-							magnis dis parturient montes.</p>
-					</div>
-				</div>
-				<div class="col-md-4 w3ls_news_grid">
-					<div class="w3layouts_news_grid">
-						<img src="images/5.jpg" alt=" " class="img-responsive" />
-						<div class="w3layouts_news_grid_pos">
-							<div class="wthree_text"><h3>plantation</h3></div>
-						</div>
-					</div>
-					<div class="agileits_w3layouts_news_grid">
-						<ul>
-							<li><i class="fa fa-calendar" aria-hidden="true"></i>30 March 2017</li>
-							<li><i class="fa fa-user" aria-hidden="true"></i><a href="#">Admin</a></li>
-						</ul>
-						<h4><a href="#" data-toggle="modal" data-target="#myModal">Satisfied Customers</a></h4>
-						<p>Vivamus lacinia odio ut euismod eleifend. Cum sociis natoque penatibus et 
-							magnis dis parturient montes.</p>
-					</div>
-				</div>
-				<div class="clearfix"> </div>
-			</div>
-		</div>
-	</div>
-<!-- //news -->
-
-<!-- banner-bottom -->
-	<div class="banner-bottom">
-		<div class="col-md-4 agileits_banner_bottom_left">
-			<div class="agileinfo_banner_bottom_pos">
-				<div class="w3_agileits_banner_bottom_pos_grid">
-					<div class="col-xs-4 wthree_banner_bottom_grid_left">
-						<div class="agile_banner_bottom_grid_left_grid hvr-radial-out">
-							<i class="fa fa-pagelines" aria-hidden="true"></i>
-						</div>
-					</div>
-					<div class="col-xs-8 wthree_banner_bottom_grid_right">	
-						<h4>Free Consultation</h4>
-						<p>Morbi viverra lacus commodo felis semper, eu iaculis lectus feugiat.</p>
-					</div>
-					<div class="clearfix"> </div>
-				</div>
-			</div>
-		</div>
-		<div class="col-md-4 agileits_banner_bottom_left1">
-			<div class="agileinfo_banner_bottom_pos">
-				<div class="w3_agileits_banner_bottom_pos_grid">
-					<div class="col-xs-4 wthree_banner_bottom_grid_left">
-						<div class="agile_banner_bottom_grid_left_grid hvr-radial-out">
-							<i class="fa fa-certificate" aria-hidden="true"></i>
-						</div>
-					</div>
-					<div class="col-xs-8 wthree_banner_bottom_grid_right">	
-						<h4>Certified Products</h4>
-						<p>Morbi viverra lacus commodo felis semper, eu iaculis lectus feugiat.</p>
-					</div>
-					<div class="clearfix"> </div>
-				</div>
-			</div>
-		</div>
-		<div class="col-md-4 agileits_banner_bottom_left2">
-			<div class="agileinfo_banner_bottom_pos">
-				<div class="w3_agileits_banner_bottom_pos_grid">
-					<div class="col-xs-4 wthree_banner_bottom_grid_left">
-						<div class="agile_banner_bottom_grid_left_grid hvr-radial-out">
-							<i class="fa fa-yelp" aria-hidden="true"></i>
-						</div>
-					</div>
-					<div class="col-xs-8 wthree_banner_bottom_grid_right">	
-						<h4>Free Helpline</h4>
-						<p>Morbi viverra lacus commodo felis semper, eu iaculis lectus feugiat.</p>
-					</div>
-					<div class="clearfix"> </div>
-				</div>
-			</div>
-		</div>
-		<div class="clearfix"> </div>
-	</div>
-<!-- //banner-bottom -->
 <!-- newsletter -->
 	<div class="newsletter">
 
