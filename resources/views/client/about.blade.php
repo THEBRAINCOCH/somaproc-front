a9bd49f09df73fb7dfc12c8b913193b0f64f3814
mraw <marwintos@gmail.com>
2017-04-25 17:42:10 +0200

    somaproc

--- resources/views/client/about.blade.php
+++ resources/views/client/about.blade.php
@@ -25,7 +25,7 @@
 			<div class="w3_agile_image">
 				<img src="images/1.png" alt=" " class="img-responsive" />
 			</div>
-			<p class="agile_para">Présentation</p>
+			
 			<div class="w3ls_news_grids"> 
 				<div class="col-md-4 w3_agile_about_grid_left">
 					<h3 style=" font-size:20px;
@@ -33,7 +33,8 @@
 	                           font-weight:400;
 	                           line-height: 1.8em;
 	                           margin-bottom: 1em;
-	                           text-align: justify;" >Somaproc
+	                           text-align: justify;" >
+	                           <span >Somaproc</span>
 	                </h3>
 					<p style=" font-size:14px;
 	                           color:#312F2F;
@@ -41,16 +42,36 @@
 	                           line-height: 1.8em;
 	                           margin-bottom: 1em;
 	                           text-align: justify;">
-	                           La Société a pour objet l'implantation et la gestion des marchés de production de Sidi Bouzid destiné à la vente en gros des produits agricole destinés aux producteurs afin de faciliter la collecte, la standardisation, le conditionnement, la conservation et la formation des prix des produits agricoles ainsi que l'exercice de toute activité rentrant dans le cadre de l'objet social précité ou qui lui sont connexes et généralement toutes opérations industrielles commerciales ou financières, mobilières ou immobilières se rattachant directement ou indirectement à l'objet social ainsi que toutes opérations annexes pouvant présenter de l'utilité pour la société, favoriser ou développer ses intérêts.
+	                           La SOMAPROC est une Société Anonyme qui opére dans le domaine de l'agroalimentaire et qui  à pour objectif la valorisation des produits agricoles et l'organisation de leurs circuits de distribution.
+                               Cette société publique à été creée en 2016 a Sidi Bouzid.
+	                </p>
+	                <h3 style=" font-size:20px;
+	                           color:#312F2F;
+	                           font-weight:400;
+	                           line-height: 1.8em;
+	                           margin-bottom: 1em;
+	                           text-align: justify;" >
+	                           <span >SIEGE SOCIAL</span>
+	                </h3>
+	                <p style=" font-size:14px;
+	                           color:#312F2F;
+	                           font-weight:400;
+	                           line-height: 1.8em;
+	                           margin-bottom: 1em;
+	                           text-align: justify;">
+	                          Le siège sociale de la société est fixé au Siège du Gouvernorat de Sidi-Bouzid - Avenu Habib Bourguiba - 9100 Sidi Bouzid.
+                               Il pourra etre transféré à tout autre endroit par déscision de l'Assmblée Générale Extraordinaire des actionnaires.
 	                </p>
 				</div>
+				
 				<div class="col-md-4 w3_agile_about_grid_left">
 					<h3 style=" font-size:20px;
 	                           color:#312F2F;
 	                           font-weight:400;
 	                           line-height: 1.8em;
 	                           margin-bottom: 1em;
-	                           text-align: justify;" >Somaproc
+	                           text-align: justify;" >
+	                           <span >OBJET</span>
 	                </h3>
 					<p style=" font-size:14px;
 	                           color:#312F2F;
@@ -61,13 +82,15 @@
 	                           La Société a pour objet l'implantation et la gestion des marchés de production de Sidi Bouzid destiné à la vente en gros des produits agricole destinés aux producteurs afin de faciliter la collecte, la standardisation, le conditionnement, la conservation et la formation des prix des produits agricoles ainsi que l'exercice de toute activité rentrant dans le cadre de l'objet social précité ou qui lui sont connexes et généralement toutes opérations industrielles commerciales ou financières, mobilières ou immobilières se rattachant directement ou indirectement à l'objet social ainsi que toutes opérations annexes pouvant présenter de l'utilité pour la société, favoriser ou développer ses intérêts.
 	                </p>
 				</div>
+				
 				<div class="col-md-4 w3_agile_about_grid_left">
 					<h3 style=" font-size:20px;
 	                           color:#312F2F;
 	                           font-weight:400;
 	                           line-height: 1.8em;
 	                           margin-bottom: 1em;
-	                           text-align: justify;" >Somaproc
+	                           text-align: justify;" >
+	                           <span >DUREE</span>
 	                </h3>
 					<p style=" font-size:14px;
 	                           color:#312F2F;
@@ -75,16 +98,35 @@
 	                           line-height: 1.8em;
 	                           margin-bottom: 1em;
 	                           text-align: justify;">
-	                           La Société a pour objet l'implantation et la gestion des marchés de production de Sidi Bouzid destiné à la vente en gros des produits agricole destinés aux producteurs afin de faciliter la collecte, la standardisation, le conditionnement, la conservation et la formation des prix des produits agricoles ainsi que l'exercice de toute activité rentrant dans le cadre de l'objet social précité ou qui lui sont connexes et généralement toutes opérations industrielles commerciales ou financières, mobilières ou immobilières se rattachant directement ou indirectement à l'objet social ainsi que toutes opérations annexes pouvant présenter de l'utilité pour la société, favoriser ou développer ses intérêts.
+	                           La durée de la société est fixée à 99 années à compter de sa constitution définitive,saufe les cas de prorogation ou de dissolution anticipée prévvuues aux présents statuts.
+	                </p>
+	                <h3 style=" font-size:20px;
+	                           color:#312F2F;
+	                           font-weight:400;
+	                           line-height: 1.8em;
+	                           margin-bottom: 1em;
+	                           text-align: justify;" >
+	                           <span >FORMATION</span>
+	                </h3>
+					<p style=" font-size:14px;
+	                           color:#312F2F;
+	                           font-weight:400;
+	                           line-height: 1.8em;
+	                           margin-bottom: 1em;
+	                           text-align: justify;">
+	                           Entre toutes les personnes qui seront propriétaires d'actions ci-après créées, et celles qui pourront l'être ultérieurement, il est formé une Société Anonyme qui sera régie par les lois en vigueur et par les présents statuts.
 	                </p>
 				</div>
+
 				<div class="col-md-6 w3_agile_about_grid_left">
-					<h3 style=" font-size:20px;
+					
+	                <h3 style=" font-size:20px;
 	                           color:#312F2F;
 	                           font-weight:400;
 	                           line-height: 1.8em;
 	                           margin-bottom: 1em;
-	                           text-align: justify;" >Somaproc
+	                           text-align: justify;" >
+	                           <span >DÉNOMAINATION</span>
 	                </h3>
 					<p style=" font-size:14px;
 	                           color:#312F2F;
@@ -92,7 +134,9 @@
 	                           line-height: 1.8em;
 	                           margin-bottom: 1em;
 	                           text-align: justify;">
-	                           La Société a pour objet l'implantation et la gestion des marchés de production de Sidi Bouzid destiné à la vente en gros des produits agricole destinés aux producteurs afin de faciliter la collecte, la standardisation, le conditionnement, la conservation et la formation des prix des produits agricoles ainsi que l'exercice de toute activité rentrant dans le cadre de l'objet social précité ou qui lui sont connexes et généralement toutes opérations industrielles commerciales ou financières, mobilières ou immobilières se rattachant directement ou indirectement à l'objet social ainsi que toutes opérations annexes pouvant présenter de l'utilité pour la société, favoriser ou développer ses intérêts.
+	                           La société prend la dénomaination de:« Société de Marché de production du Centre ».
+                               Outre cette dénomaination, la société peut, indifférement, séparément et uniquement utiliser la dénomaination en abrégé SOMAPROC.
+                               Tous les actes et documents émanat de la société doivent mentionner la dénomaination sociale, précédée ou suivie immédiatement des mots « société anonyme » ou des initiales « S.A. », de lénonciation du montant du capital social et des numéros et lieu de son immatriculation au Registre de Commerce.
 	                </p>
 				</div>
 				<div class="col-md-6 w3_agile_about_grid_right">
@@ -153,7 +197,7 @@
 <!-- about-bottom -->
 	<div class="about-bottom">
 		<div class="container">
-			<h3><span>SOMAPROC</span> est la solution la plus appropriée pour les produits agricoles en <span>Tunisie</span>.</h3>
+			<h3><span>SOMAPROC</span> est la solution la plus appropriée pour la valorisation produits agricoles en <span>Tunisie</span>.</h3>
 			<div class="agileits_w3layouts_learn_more agileits_learn_more hvr-radial-out">
 				<a href="#" data-toggle="modal" data-target="#myModal">Read More</a>
 			</div>
